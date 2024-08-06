b6c811fe3793f6fbfeda8eeb4c1298dbb40eb131
Kartikeya Joshi <kartikeyajoshi1234@gmail.com>
2024-08-07 00:22:00 +0530

    Added About Information

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import About from './components/About';
+import Alerts from './components/Alerts';
 
 function App() {
   return (
     <>
   <Navbar title={"TextUtils"} />
   <TextForm heading={"Enter your text below"} />
+  <About />
+  <Alerts />
   </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
 
 export default function TextForm(props) {
-    const [text, setText] = useState();
+    const [text, setText] = useState("");
     const handleOnChange = (event) => {
         let newText = event.target.value.toUpperCase();
         setText(newText);
+        if (text.length > 20) {
+            setText(event.target.value.slice(0, -2))
+            alert("Max allowed characters are "+ (text.length-1));
+        }
     }
   return (
     <>
@@ -12,6 +16,7 @@ export default function TextForm(props) {
     <div className="mb-3 pt-2">
     <center><h1>{props.heading}</h1></center>
     <textarea className="form-control" id="textBox" rows="5" placeholder="Enter Text" onChange={handleOnChange} value={text}></textarea>
+    <p>Words: {text.split(" ").length}<br />Characters: {text.length}</p>
     </div>
     </div>
     </>
